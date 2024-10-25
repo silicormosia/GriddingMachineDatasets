19e11114d87e013cb6aea4ffaec68bc623512e99
Yujie Wang <jesiner@gmail.com>
2024-10-24 17:37:00 -0700

    add pipeline for MCD43A4_V1

--- test/preparation/gee/MCD43A4_061.jl
+++ test/preparation/gee/MCD43A4_061.jl
@@ -28,10 +28,19 @@ function combine_bands(y::Int, nx::Int, mt::String, b::Int)
     filepath = "$(INPUT_FOLDER)/$(y)_$(nx)X_$(mt).tif";
     bands = (collect(1:ind_dim(mt)) .- 1) .* 7 .+ b;
 
+    # first index is 2 when y == 2000 and mt == 1M (because MODIS started collecting data since 2000-Feb)
+    first_ind = if y == 2000 && mt == "1M"
+        2
+    elseif y == 2000 && mt == "8D"
+        7
+    else
+        1
+    end;
+
     # loop through the time index
-    data = ones(Float32, lon_dim(nx), lat_dim(nx), ind_dim(mt));
-    for i in 1:ind_dim(mt)
-        band_data = GMD.read_geotiff(filepath, bands[i]);
+    data = ones(Float32, lon_dim(nx), lat_dim(nx), ind_dim(mt)) .* NaN32;
+    for i in first_ind:ind_dim(mt)
+        band_data = GMD.read_geotiff(filepath, bands[i-first_ind+1]);
         data[:,:,i] .= band_data;
     end;
 
@@ -40,7 +49,7 @@ end;
 
 
 # 2. save the data to netCDF file per band
-for y in 2002:2024
+for y in 2000:2024
     for nx in [1]
         for mt in ["8D", "1M"]
             in_file = "$(INPUT_FOLDER)/$(y)_$(nx)X_$(mt).tif";
