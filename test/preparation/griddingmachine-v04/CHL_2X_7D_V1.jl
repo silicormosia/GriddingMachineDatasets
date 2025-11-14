521cdaceae23302732abbf2cbdab745f77e56828
Yujie <jesiner@gmail.com>
2025-11-14 15:41:52 +0800

    test new script

---
+++ test/preparation/griddingmachine-v04/CHL_2X_7D_V1.jl
@@ -0,0 +1,26 @@
+using GriddingMachine.Collector: download_artifact!
+using NetcdfIO: append_nc!, create_nc!, read_nc
+
+
+
+lon = read_nc(download_artifact!("CHL_2X_7D_V1"), "lon");
+lat = read_nc(download_artifact!("CHL_2X_7D_V1"), "lat");
+ind = read_nc(download_artifact!("CHL_2X_7D_V1"), "ind");
+data = read_nc(download_artifact!("CHL_2X_7D_V1"), "data");
+stdv = read_nc(download_artifact!("CHL_2X_7D_V1"), "std");
+data_filled = deepcopy(data);
+
+# fill the missing values with mean value
+# use land mask to identify missing values
+land_mask = read_nc(download_artifact!("LM_4X_1Y_V1"), "data");
+
+
+# copy a nanmean
+
+
+# save the data
+new_data_folder = joinpath(homedir(), "GriddingMachine/refactored");
+new_data_file = joinpath(new_data_folder, "CHL_2X_7D_V1_R1.nc");
+create_nc!(new_data_file, ["lon", "lat", "ind"], [size(data)...]);
+append_nc!(new_data_file, "data", data_filled, Dict{String,Any}("about" => "Leaf chlorophyll content"), ["lon", "lat", "ind"]);
+append_nc!(new_data_file, "std", stdv, Dict{String,Any}("about" => "Standard deviation of leaf chlorophyll content"), ["lon", "lat", "ind"]);
